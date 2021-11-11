7edc3a89d8cb51aad8e2c5c3225de74a1bbc4479
Renzo Samuel García Cervera <renzogc745@gmail.com>
2021-11-11 11:49:06 -0500

    Creando objeto como BD

--- src/components/CrudApp.js
+++ src/components/CrudApp.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React from "react";
+
+const frameworksDB = [
+  {
+    id: 1,
+    name: "Angular",
+    url: "https://angular.io",
+  },
+  {
+    id: 2,
+    name: "React",
+    url: "https://es.reactjs.org/",
+  },
+  {
+    id: 3,
+    name: "Vue",
+    url: "https://vuejs.org/",
+  }
+];
 
 export default function () {
-    return(
-        <div>
-            <h2>CRUD</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2>CRUD</h2>
+    </div>
+  );
 }
